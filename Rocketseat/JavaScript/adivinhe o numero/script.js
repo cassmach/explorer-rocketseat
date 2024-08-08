22e5f1d1e2a3b30ac08a7c56a1e0726f23bf7a58
cass.mach <cassiomachado1995@outlook.com>
2024-08-08 09:46:52 -0300

    up

--- Rocketseat/JavaScript/adivinhe o numero/script.js
+++ Rocketseat/JavaScript/adivinhe o numero/script.js
@@ -3,11 +3,22 @@ let result = prompt('Advinhe o número que estou pensando? Está entre 0 e 10')
 const randomNumber = Math.round(Math.random() * 10) ;
        // deixar valor (1) abaixo para fazer a conta de tentativas correta
 let xAttempts = 1;
+let singleAttempts = 'Tentativa';
      // Precisa colocar o (Number) porque result antes era uma string para o comparativo
 while ( Number(result) != randomNumber) {
    result =  prompt('Erro, tente novamente')
    // se entrar aqui vamos somar uma tentativa
    xAttempts++
+
+
+if (xAttempts > 1) {
+ singleAttempts = 'Tentativas'     
+} else {
+ singleAttempts = 'Tentativa'
 }
 
-alert(`Parabenso numero que eu pensei foi ${randomNumber}! Você advinhou o número em ${xAttempts} tentativas`)
+   
+}
+
+alert(`Parabens numero que eu pensei foi ${randomNumber}! Você advinhou o número em ${xAttempts} ${singleAttempts}`)
+
