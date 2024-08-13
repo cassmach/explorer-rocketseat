8ccb2a4dc4382869e8874f8b893d77780b86e3fe
cass.mach <cassiomachado1995@outlook.com>
2024-08-13 13:02:32 -0300

    up

--- Rocketseat/JavaScript/calculando IMC/script.js
+++ Rocketseat/JavaScript/calculando IMC/script.js
@@ -24,9 +24,24 @@ const patients = [
     },
 ];
 
+// Função só do cálculo
+function IMC (weight, heigth) {
+   return  (weight / (heigth ** 2)).toFixed(2)
+}
+
+
+// aqui criação a função onde ele busca e retorna o array 
+// const printPatientImc = function (patients) {...    Modelo de função anonima
+// const printPatientImc =  (patients) => {...  Modelo de Arrow anonima
+function printPatientImc(patients) {
+    return (`
+        Paciente ${patients.name} possui o IMC de ${IMC(patients.weight, patients.heigth)}
+        `)
+}
 
 
 
-alert (`
-   Paciente ${patients[0].name} possui o IMC de ${(patients[0].weight / (patients[0].heigth ** 2)).toFixed(2)}
-   `)
+for ( let patient of patients) {  // criação de um loop FOR OF para não precisar ficar chamando a função manualmente
+    let imcMessage = printPatientImc(patient)
+   console.log(imcMessage)
+};
