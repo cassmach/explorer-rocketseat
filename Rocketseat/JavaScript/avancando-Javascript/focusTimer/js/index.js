d382f15373b9773631ad0dc77be1e268f7f506dd
cass.mach <cassiomachado1995@outlook.com>
2024-08-27 17:32:02 -0300

    up

--- Rocketseat/JavaScript/avancando-Javascript/focusTimer/js/index.js
+++ Rocketseat/JavaScript/avancando-Javascript/focusTimer/js/index.js
@@ -1,3 +1,8 @@
+// default import
+import Controls from "./controls.js"; 
+//named import
+import Timer  from "./timer.js";
+
 // variables
 const buttonPlay = document.querySelector('.play');
 const buttonPause = document.querySelector('.pause');
@@ -5,44 +10,39 @@ const buttonStop = document.querySelector('.stop');
 const buttonSet = document.querySelector('.set');
 const buttonSoundOn = document.querySelector('.sound-on');
 const buttonSoundOff = document.querySelector('.sound-off');
-let minutes;
 const minutesDisplay = document.querySelector('.minutes');
 const secondsDisplay = document.querySelector('.seconds')
 
-function countdown () {
-    setTimeout(function() {
-        let seconds = Number(secondsDisplay.textContent) 
-        
-        if (seconds <= 0) {
-            seconds = 60
-        }
 
-        secondsDisplay.textContent = seconds -1
-        countdown ()
-    }, 1000)
+const controls = Controls ({
+    buttonPlay,
+    buttonPause,
+    buttonStop,
+    buttonSet
+})
 
-}
+const timer = Timer ({
+    minutesDisplay,
+    secondsDisplay,
+    resetControls: controls.reset,
+})
+
 
 // events
 buttonPlay.addEventListener('click', function() {
-    buttonPlay.classList.add('hide')
-    buttonPause.classList.remove('hide')
-    buttonSet.classList.add('hide')
-    buttonStop.classList.remove('hide')
-
-    countdown()
+    controls.play()
+    timer.countdown()
 
 });
 
 buttonPause.addEventListener('click', function() {
-    buttonPause.classList.add('hide')
-    buttonPlay.classList.remove('hide') 
+    controls.pause()
+    timer.hold()
 })
 buttonStop.addEventListener('click', function() {
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
-    buttonSet.classList.remove('hide')
-    buttonStop.classList.add('hide')
+    controls.reset()
+    timer.reset()
+    
 })
 
 buttonSoundOn.addEventListener('click', function() {
@@ -55,6 +55,13 @@ buttonSoundOff.addEventListener('click', function() {
 })
 
 buttonSet.addEventListener('click', function() {
-    minutes = prompt('Quantos minutos?');
-    minutesDisplay.textContent = minutes
-})
+    let newMinutes = controls.getMinutes()
+
+    if(!newMinutes) {
+        timer.reset()
+        return
+    }
+
+    timer.updateDisplay(newMinutes, 0) 
+    timer.updateMinutes(newMinutes)
+});
